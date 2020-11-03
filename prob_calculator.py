678cfac330ec9b2a5f2cf1f3d47076c43543c585
pkarczma <przemyslaw.karczmarczyk@cern.ch>
2020-11-03 17:01:36 +0000

    Variable names changed and added comments

--- prob_calculator.py
+++ prob_calculator.py
@@ -8,19 +8,22 @@ class Hat:
 
     def __init__(self, **kwargs):
         self.contents = []
+        # Add contents of the hat based on the parameters provided
         for color, value in kwargs.items():
             for val in range(value):
                 self.contents.append(color)
 
+    # Method for drawing from the hat
     def draw(self, nballs):
+        # Take the whole hat in case we request for all balls
         if nballs >= len(self.contents):
-            balls_list = copy.deepcopy(self.contents)
+            balls_drawn = copy.deepcopy(self.contents)
             self.contents = []
-            return balls_list
-        balls_list = random.sample(self.contents, nballs)
-        for ball in balls_list:
+            return balls_drawn
+        balls_drawn = random.sample(self.contents, nballs)
+        for ball in balls_drawn:
             self.contents.remove(ball)
-        return balls_list
+        return balls_drawn
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     num_successes = 0
