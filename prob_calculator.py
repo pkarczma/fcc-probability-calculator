9c62d4855af73378b1500b70a6871aa084d860f6
pkarczma <przemyslaw.karczmarczyk@cern.ch>
2020-11-03 16:38:07 +0000

    Added experiment function

--- prob_calculator.py
+++ prob_calculator.py
@@ -17,11 +17,22 @@ class Hat:
             balls_list = copy.deepcopy(self.contents)
             self.contents = []
             return balls_list
-        random.seed(0)
         balls_list = random.sample(self.contents, nballs)
         for ball in balls_list:
             self.contents.remove(ball)
         return balls_list
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    return
+    num_successes = 0
+    for iexperiment in range(num_experiments):
+        random.seed(iexperiment)
+        full_hat = copy.deepcopy(hat)
+        balls_drawn = full_hat.draw(num_balls_drawn)
+        success = True
+        for color, value in expected_balls.items():
+            if balls_drawn.count(color) < value:
+                success = False
+                break
+        if success == True:
+            num_successes += 1
+    return num_successes / num_experiments
